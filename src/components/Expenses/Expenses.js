de4b6a09eec8b362933b1bddf08a93e6a42cb6ce
Juan Hoyos <juanse.jsh@gmail.com>
2023-09-20 22:04:16 -0400

    reactive list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,16 +11,26 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((item) =>
+    item.date.getFullYear().toString() === filteredYear,
+  );
+
+  let expenseContent = <p>No expenses...</p>;
+
+  if (filteredExpenses.length > 0 ) {
+    expenseContent = filteredExpenses.map((expense) =>
+      <ExpenseItem key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}/>,
+    );
+  }
+
   return (
     <div>
       <Card className="expenses">
         <ExpenseFiler selected={filteredYear} onFilter={filterHandler}/>
-        <ExpenseItem title={props.items[0].title}
-          amount={props.items[0].amount} date={props.items[0].date}/>
-        <ExpenseItem title={props.items[1].title}
-          amount={props.items[1].amount} date={props.items[1].date}/>
-        <ExpenseItem title={props.items[2].title}
-          amount={props.items[2].amount} date={props.items[2].date}/>
+        {expenseContent}
       </Card>
     </div>
   );
